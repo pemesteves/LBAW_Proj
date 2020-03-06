71c9d8f2ed0f63a993d8f0dde5a5e81a176db44b
BerserkingIdiot <skullventuzelos@hotmail.com>
2020-03-06 08:12:47 +0000

    some css for messages page added

--- html/templates/template_message.php
+++ html/templates/template_message.php
@@ -1,10 +1,10 @@
 <?php
 function draw_chat_info($image_src, $image_alt, $name, $num_mess){
 ?>
-    <article class="card-mb-3">
+    <article class="card-mb-3" style="border-color: sandybrown; border-width: 0; border-bottom-width: 0.1em; border-style: solid; width: 100%; height: 3em; padding: 0.25em">
     <div class="row">
         <div class="col-md-4">
-            <img class="card-img" src="<?=$image_src?>" alt="<?=$image_alt?>" style="width:2em"/>
+            <img class="card-img" src="<?=$image_src?>" alt="<?=$image_alt?>" style="width:2.5em"/>
         </div>
         <div class="col-md-8">
             <h2 class="card-title"><?=$name?>
@@ -26,9 +26,12 @@ function draw_chat_info($image_src, $image_alt, $name, $num_mess){
 
 function draw_message($owner, $message){
 ?>
-<li class="<?=$owner ? "my_message" : "other_message"?>">
-    <p><?=$message?></p>
-</li>
+<p <?php if($owner) { ?> class="my_message" style="width:max-content; align-self:end; padding:0.5em; border-radius:1em; text-align:right; background-color:sandybrown; color: white; text-decoration:none"
+    <?php } 
+    else { ?> class="other_message" style="width:max-content; align-self:start; padding:0.5em; border-radius:1em; text-align:left; background-color:whitesmoke; color: black; text-decoration:none"
+    <?php } ?> >
+    <?=$message?>
+</p>
 <?php
 }
 ?>
