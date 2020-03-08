b5512fa8f2e49f6a4fcdd199a267b2ac93008cfe
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-03-08 16:07:00 +0000

    Groups changes

--- html/group.php
+++ html/group.php
@@ -7,12 +7,21 @@ include_once 'templates/template_group.php';
 draw_header();
 draw_navbar();
 
-draw_group_card();
+$name = "NIAEFEUP";
+$members = "50";
+draw_group_card($name, $members);
 draw_create_post();
 
-draw_post_card();
-draw_post_card();
-draw_post_card();
+
+$author = "Matias";
+$uni = "FEUP";
+$date = "03-03-2020";
+$hour = "10:28";
+$title = "Jantar NIAEFEUP";
+$post_content = "Caros colegas, marcamos um jantar para os membros do NIAEFEUP no dia 20 de Março de 2020, pelas 20:30h no restaurante X. O preço será de 10$. Os interessados que se inscrevam!
+Cumprimentos, Matias.";
+draw_post_card($author, $uni, $date, $hour, $title, $post_content);
+
 
 draw_footer();
 ?>
