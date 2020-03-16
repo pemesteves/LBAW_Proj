51ef2652c224a6b56f4634d3d210f361be11c840
Pedro Esteves <up201705160@fe.up.pt>
2020-03-16 14:27:00 +0000

    Changed buttons size in index.php

--- html/index.php
+++ html/index.php
@@ -16,12 +16,12 @@ draw_header();
             <img src="./images/init.png" alt="init" style="position:absolute;bottom:0%;object-fit:fill;width: 100%; height: 50%";>
             <h4 style="display:block;position:absolute;top:15%;left:2%; font-size: 2em;">Connecting U to U</h4>
             <a href="login.php" style="color:inherit"> 
-                <button style="display:block;position:absolute;top:30%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;">
+                <button style="display:block;position:absolute;top:30%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold; height: 3.25em;">
                     LOGIN 
                 </button>
             </a>
             <a href="register.php" style="color:inherit">
-                <button style="display:block;position:absolute;top:40%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;"> 
+                <button style="display:block;position:absolute;top:40%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold; height: 3.25em;"> 
                     REGISTER 
                 </button>
             </a>
