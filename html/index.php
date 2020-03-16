7b992a1ff6368fddfda8c8da3fd08f1b414135de
Pedro Esteves <up201705160@fe.up.pt>
2020-03-16 14:24:26 +0000

    Changed links in index and login headers. Changed buttons in index page

--- html/index.php
+++ html/index.php
@@ -15,12 +15,16 @@ draw_header();
         <div id="init_page_area" style="width:100%;height:100%;">
             <img src="./images/init.png" alt="init" style="position:absolute;bottom:0%;object-fit:fill;width: 100%; height: 50%";>
             <h4 style="display:block;position:absolute;top:15%;left:2%; font-size: 2em;">Connecting U to U</h4>
-            <button style="display:block;position:absolute;top:30%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;">
-                <a href="login.php" style="color:inherit"> LOGIN </a>
-            </button>
-            <button style="display:block;position:absolute;top:40%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;"> 
-                <a href="register.php" style="color:inherit"> REGISTER </a> 
-            </button>
+            <a href="login.php" style="color:inherit"> 
+                <button style="display:block;position:absolute;top:30%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;">
+                    LOGIN 
+                </button>
+            </a>
+            <a href="register.php" style="color:inherit">
+                <button style="display:block;position:absolute;top:40%;right:15%;border:none;border-radius:10em;background-color:#f68e4d;width:200px;height:60px;text-align:center;color:white; font-weight: bold;"> 
+                    REGISTER 
+                </button>
+            </a>
         </div>
 
 
--- html/register.php
+++ html/register.php
@@ -5,7 +5,7 @@ draw_header();
 
 ?>
         <nav class="navbar navbar-dark" style="background-color: sandybrown;"> <!-- #ffa31a -->
-            <a class="navbar-brand" href="#.php">
+            <a class="navbar-brand" href="./index.php">
                 <h1 style="color: whitesmoke;">UConnect <span class="fa fa-graduation-cap"></span></h1>
                 </a> <!-- whitesmoke -->
         </nav>
