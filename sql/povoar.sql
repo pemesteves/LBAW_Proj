d99d114afb738d4bdc529f1a2564f576fdb8d3e0
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-03-25 22:04:11 +0000

    mais povoar

--- sql/povoar.sql
+++ sql/povoar.sql
@@ -7,9 +7,26 @@ insert into public."user" ("user_id", "name", "email", "password", TYPE) values(
 insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Pedro Esteves', 'pmesteves@feup.pt', '80cfa04f6ef471fef3a12581ca0ef857af748aa19463ea0292e4a9c310a4dae9', NULL) --estevesboss
 insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Vitor Ventuzelos', 'berserking@feup.pt', '979d2b82d1ed2c6f23406e898b9adb9060090501928654278eefff39a9fe15f4', NULL) --berserking_idiot
 insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'José Martins', 'martins@flup.pt', '4447a0a857c5856a65be0b6d88e522930d60cc70cd87fc5a97ef5f9b29472e1f', NULL) --martins
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Marta Camões', 'mcamoes@fcup.pt', '919591ee58ee6ce7cec959d2a0d9130e7cf111d7ddafa15dc5283d74a2cb3a2a', NULL) --123p
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Diana Magalhães', 'dmaga@flup.pt', 'b3d17ebbe4f2b75d27b6309cfaae1487b667301a73951e7d523a039cd2dfe110', NULL) --password12
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Tiago Pessoa', 'tpessoa@isep.pt', '2c8c79ee2d9b918b6839430f08cef4c811316c33d06560c7223975e3496fb20f', NULL) --hacked91
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Ricardo Pinto', 'rpinto@fmup.pt', '36fa35b7423a57975566cccb407a11667c166b92a7a6b8bf1a0e9d8accf8c417', NULL) --debugger99
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Maria Soares', 'msoares@icbas.pt', 'a36d932abb39c80c3f573c08b7a2260e0555d053eeb9cacab9a3406b02d2330b', NULL) --12345p
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Francisco Costa', 'fcosta@ffup.pt', '1ba3d16e9881959f8c9a9762854f72c6e6321cdd44358a10a4e939033117eab9', NULL) --pass2
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'José Silva', 'runcolho@fadeup.pt', 'e6c3da5b206634d7f3f3586d747ffdb36b5c675757b380c6a5fe5c570c714349', NULL) --pass1
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Miguel Alvim', 'malvim21@fadeup.pt', 'cb71cd3f59909532b783257f5f97d0502af258231f9d17cbe762799c0bb4a0f6', NULL) --pass0
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Dinis Pereira', 'dinpereira@fmup.pt', '1958e0a5696c86a9a147b27eb7b5c828fd8f99921fb7a1c068fb2d4860f85cd0', NULL) --lbaw2034
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Soraia Tavares', 'stavares@ffup.pt', '7d9187aec886a65a920d3b4a2e3eb66c771510255de4f02a1b0c67a1be856c33', NULL) --lbaw
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Osvaldo Antunes', 'oantunes@fadeup.pt', 'adf1a868d88c0f53317e0dcbfb982c19438f5c46c14725dc24e837bd75afb0cb', NULL) --query10
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Mariana Castro', 'mcastro@fcup.pt', 'a2979efbe3ef341d34ba03d1493da9d9282c2906710f33311e72dfe0b67b7373', NULL) --sql2
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Ana Faria', 'afaria@fep.pt', '0fa83a5d1a9db56f287856c263fb94c8d579fd8916750d67f7b78d1919dfa7e5', NULL) --turbulencia2725
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Rui Cardoso', 'rcardoso@fep.pt', 'bbe41015c3f1053c527579a7a45b5531c93ac11c7d14664465b68903a6736e34', NULL) --bazofi10
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'AEFEUP', 'aefeup@feup.pt', '6b82542b8c0a0afa1278f6c5a23d05aaf94eafec58a01cf70be8aa755cb26cce', NULL) --gustavoboss
+insert into public."user" ("user_id", "name", "email", "password", TYPE) values(NULL, 'Admin2', 'admin2@admins.pt', '25efc3e7a0aa2e33c3ffd37792156d9bb7e6eece07e268fe4bb558c70ce9cec2', NULL) --adminGang
 
 
 insert into public."admin" ("admin_id", "user_id") values (1, 1);
+insert into public."admin" ("admin_id", "user_id") values (NULL, 24);
 
 
 insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (1, 2, 'Just one regular user!');
@@ -18,7 +35,22 @@ insert into public."regular_user" ("regular_user_id", "user_id", "personal_info"
 insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 5, 'DB user');
 insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 6, 'GameJam user');
 insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 7, 'Another one!');
-insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 8, 'Flup student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 8, 'FLUP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 9, 'FCUP teacher');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 10, 'FLUP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 11, 'I am an ISEP teacher');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 12, 'FMUP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 13, 'ICBAS student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 14, 'FFUP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 15, 'Sports student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 16, 'FADEUP teacher');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 17, 'FMUP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 18, 'Pharmacy student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 19, 'Sports student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 20, 'Science student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 21, 'FEP student');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 22, 'FEP teacher');
+insert into public."regular_user" ("regular_user_id", "user_id", "personal_info") values (NULL, 23, 'AEFEUP');
 
 
 insert into public."student" ("student_id", "regular_user_id") values (1, 2);
@@ -26,33 +58,68 @@ insert into public."student" ("student_id", "regular_user_id") values (NULL, 5);
 insert into public."student" ("student_id", "regular_user_id") values (NULL, 6);
 insert into public."student" ("student_id", "regular_user_id") values (NULL, 7);
 insert into public."student" ("student_id", "regular_user_id") values (NULL, 8);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 10);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 12);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 13);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 14);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 15);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 17);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 18);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 19);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 20);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 21);
+insert into public."student" ("student_id", "regular_user_id") values (NULL, 22);
 
 
 insert into public."teacher" ("teacher_id", "regular_user_id") values (1, 3);
+insert into public."teacher" ("teacher_id", "regular_user_id") values (NULL, 9);
+insert into public."teacher" ("teacher_id", "regular_user_id") values (NULL, 11);
+insert into public."teacher" ("teacher_id", "regular_user_id") values (NULL, 16);
+insert into public."teacher" ("teacher_id", "regular_user_id") values (NULL, 22);
 
 
 insert into public."organization" ("organization_id", "regular_user_id", "approval") values (1, 4, TRUE);
+insert into public."organization" ("organization_id", "regular_user_id", "approval") values (NULL, 23, TRUE);
 
 
 insert into public."event" ("event_id", "organization_id", "name", "location", "date", "information") values (1, 1, 'Evento de LBAW', 'Porto', '2020-04-23 17:45:00');
+insert into public."event" ("event_id", "organization_id", "name", "location", "date", "information") values (NULL, 2, 'AEFEUP Day', 'Porto', '2020-05-25 09:15:00');
 
 
 insert into public."group" ("group_id", "name", "information", TYPE) values (1, "Grupo de LBAW", "Grupo para os estudantes de LBAW", 'normal');
+insert into public."group" ("group_id", "name", "information", TYPE) values (NULL, "UP/IPP Group", "Estudantes do Porto", 'normal');
 
 
-insert into public."user_in_group" ("user_id", "group_id") values (2, 0);
-insert into public."user_in_group" ("user_id", "group_id") values (5, 0);
-insert into public."user_in_group" ("user_id", "group_id") values (6, 0);
-insert into public."user_in_group" ("user_id", "group_id") values (7, 0);
+insert into public."user_in_group" ("user_id", "group_id") values (2, 1);
+insert into public."user_in_group" ("user_id", "group_id") values (5, 1);
+insert into public."user_in_group" ("user_id", "group_id") values (6, 1);
+insert into public."user_in_group" ("user_id", "group_id") values (7, 1);
+insert into public."user_in_group" ("user_id", "group_id") values (2, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (5, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (6, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (7, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (8, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (9, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (10, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (12, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (13, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (14, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (15, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (17, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (18, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (19, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (20, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (21, 2);
+insert into public."user_in_group" ("user_id", "group_id") values (22, 2);
 
 
 insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (1, 2, 'FEUP LIFE', 'Gosto de estudar na FEUP', '2020-02-20 17:45:00', 4, 3, NULL, NULL, NULL);
 insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 2, 'Aula de LBAW', 'Na próxima aula de LBAW temos que entregar todos os artefactos desta componente?', '2020-02-24 10:38:43', 2, 0, NULL, NULL, NULL);
 insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 2, 'Trabalho COMP', 'Alguém da turma 4 necessita de um elemento para a realização do trabalho 1 de Compiladores?', '2020-03-05 15:50:11', 5, 1, NULL, NULL, NULL);
 insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 1, 'Inquéritos FLUP', 'Conseguem todos preencher os inquéritos lançados pela faculdade?', '2020-03-10 08:31:06', 10, 0, NULL, NULL, NULL);
-insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 3, 'Quarentena', 'Pedimos a todos os alunos da UP, em especial os do ISEP para ficarem em casa durante este período de quarentena, seguindo as indicações da DGS.', '2020-03-13 16:54:51', 40, 1, NULL, NULL, NULL);
+insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 3, 'Quarentena', 'Pedimos a todos os alunos da Academia, em especial os do ISEP para ficarem em casa durante este período de quarentena, seguindo as indicações da DGS.', '2020-03-13 16:54:51', 40, 1, NULL, NULL, NULL);
 insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 6, 'Grupo para LBAW', 'Bom dia pessoal, criei este grupo para ser mais facil partilharmos os ficheiros para LBAW. Abraço', '2020-03-12 09:21:41', 3, 0, NULL, NULL, 0);
-
+insert into public."post" ("post_id", "author_id", "title", "body", "date", "upvotes", "downvotes", TYPE, "event_id", "group_id") values (NULL, 23, 'AEFEUP Day', 'Bom dia estudantes! No próximo dia 25 de Maio realizar-se-á o AEFEUP Day, um evento recheado de brindes e empresas para construíres um futuro melhor', '2020-03-22 10:33:21', 10, 0, NULL, 2, NULL);
 
 
 insert into public."comment" ("comment_id", "user_id","post_id", "comment_to_id", "body", "date", "upvotes", "downvotes") values (1, 5, 2, NULL, 'Não, essa entrega tem uma data específica, verifica no Moodle', '2020-02-24 11:03:22', 1, 0);
@@ -61,20 +128,30 @@ insert into public."comment" ("comment_id", "user_id","post_id", "comment_to_id"
 
 
 insert into public."chat" ("chat_id") values (1);
+insert into public."chat" ("chat_id") values (NULL);
 
-insert into public."user_in_chat" ("user_id", "chat_id") values (2,0);
-insert into public."user_in_chat" ("user_id", "chat_id") values (5,0);
-insert into public."user_in_chat" ("user_id", "chat_id") values (6,0);
-insert into public."user_in_chat" ("user_id", "chat_id") values (7,0);
 
-insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (1, 2, 0, 'Boas pessoal, este vai ser o chat de LBAW', '2020-03-20 14:22:55');
-insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 5, 0, 'Boas!', '2020-03-20 14:30:00');
-insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 6, 0, 'Então, tudo bem?', '2020-03-20 15:02:21');
-insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 7, 0, 'Mãos à obra', '2020-03-20 15:03:41');
+insert into public."user_in_chat" ("user_id", "chat_id") values (2,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (5,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (6,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (7,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (3,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (9,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (11,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (16,1);
+insert into public."user_in_chat" ("user_id", "chat_id") values (22,1);
+
+
+insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (1, 2, 1, 'Boas pessoal, este vai ser o chat de LBAW', '2020-03-20 14:22:55');
+insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 5, 1, 'Boas!', '2020-03-20 14:30:00');
+insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 6, 1, 'Então, tudo bem?', '2020-03-20 15:02:21');
+insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 7, 1, 'Mãos à obra', '2020-03-20 15:03:41');
+insert into public."message" ("message_id", "sender_id", "chat_id", "body", "date") values (NULL, 9, 2, 'Criei este chat para todos os professores se poderem manter em contacto', '2020-03-22 21:16:47');
 
 
 insert into public."report" ("report_id", "reporter_id", "approval", "reason", "reported_user_id", "reported_event_id", "reported_post_id", "reported_comment_id", "reported_group_id") values (1, 3, NULL, "so para testar a ferramenta", NULL, NULL, 0, NULL, NULL);
 insert into public."report" ("report_id", "reporter_id", "approval", "reason", "reported_user_id", "reported_event_id", "reported_post_id", "reported_comment_id", "reported_group_id") values (NULL, 2, NULL, "pouco conteudo", NULL, NULL, NULL, 1, NULL);
+
 
 --FALTA NOTIFICATION
 --FALTA NOTIFIED_USER
