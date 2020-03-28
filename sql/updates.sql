98ef077c717a199f58f50731edaec4e31215ca4a
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-03-28 20:53:07 +0000

    All updates

--- sql/updates.sql
+++ sql/updates.sql
@@ -20,3 +20,68 @@
     INSERT INTO "friend"
     VALUES ($userId, $userId2, DEFAULT);
 
+--UPDATES
+    UPDATE "friend"
+    SET TYPE = 'accepted'
+    WHERE ("friend_id1" = $userId AND "friend_id2" = $userId2);
+
+    UPDATE "post"
+    SET TYPE = 'deleted'
+    WHERE "post_id" = $postId;
+    
+    UPDATE "post"
+    SET TYPE = 'blocked'
+    WHERE "post_id" = $postId;
+
+    UPDATE "user"
+    SET TYPE = 'deleted'
+    WHERE "user_id" = $userId;
+    
+    UPDATE "user"
+    SET TYPE = 'blocked'
+    WHERE "user_id" = $userId;
+
+    UPDATE "organization"
+    SET "approval" = TRUE
+    WHERE "organization_id" = $orgId;
+
+    UPDATE "group"
+    SET TYPE = 'deleted'
+    WHERE "group" = $groupId;
+    
+    UPDATE "group"
+    SET TYPE = 'blocked'
+    WHERE "group_id" = $groupId;
+
+    UPDATE "notified_user"
+    SET "seen" = TRUE
+    WHERE ("notification_id" = $notId AND "user_notified" = $userId);
+
+    UPDATE "report"
+    SET "approval" = TRUE
+    WHERE "report_id" = $reportId;
+
+    UPDATE "report"
+    SET "approval" = FALSE
+    WHERE "report_id" = $reportId;
+
+--DELETES
+    --DELETE FROM "report"
+    --WHERE "approval" = FALSE;
+
+    --DELETE FROM "friend"
+    --WHERE TYPE = 'refused'
+
+    -- ESTES DOIS PODEM SER TRIGGERS?
+
+    DELETE FROM "comment"
+    WHERE "comment_id" = $commentId;
+    
+    DELETE FROM "chat"
+    WHERE "chat_id" = $chatId;
+
+    DELETE FROM "message"
+    WHERE "message_id" = $messageId;
+
+    DELETE FROM "user_interested_in_event"
+    WHERE ("user_id" = $userId AND "event_id" = $eventId);
