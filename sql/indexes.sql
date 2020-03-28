e37e5c928bbb3d79131e264d1ecc19dc6e232024
Pedro Esteves <up201705160@fe.up.pt>
2020-03-28 20:02:20 +0000

    Updated indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,15 +1,24 @@
 DROP INDEX IF EXISTS "user_notified"; 
 DROP INDEX IF EXISTS "user_email";
+DROP INDEX IF EXISTS "student_regular_id";
+DROP INDEX IF EXISTS "teacher_regular_id";
+DROP INDEX IF EXISTS "organization_regular_id";
 DROP INDEX IF EXISTS "search_post_titles";
 DROP INDEX IF EXISTS "search_user_names";
 DROP INDEX IF EXISTS "search_group_names";
 DROP INDEX IF EXISTS "search_event_names";
 
-
+-- GENERAL INDEXES
 CREATE INDEX "user_notified" ON "notified_user"("user_notified");
 CREATE INDEX "user_email" ON "user" USING hash("email");
 
-CREATE INDEX "search_post_titles" ON "post" USING GIST("title");
-CREATE INDEX "search_user_names" ON "user" USING GIST("name");
-CREATE INDEX "search_group_names" ON "group" USING GIST("name");
-CREATE INDEX "search_event_names" ON "event" USING GIST("name");
+-- REGULAR USER ID FOREIGN KEYS INDEXES
+CREATE INDEX "student_regular_id" ON "student"("regular_user_id");
+CREATE INDEX "teacher_regular_id" ON "teacher"("regular_user_id");
+CREATE INDEX "organization_regular_id" ON "organization"("regular_user_id");
+
+-- GIST SEARCH INDEXES 
+CREATE INDEX "search_post_titles" ON "post" USING GIST(to_tsvector('english', "title"));
+CREATE INDEX "search_user_names" ON "user" USING GIST(to_tsvector('english', "name"));
+CREATE INDEX "search_group_names" ON "group" USING GIST(to_tsvector('english', "name"));
+CREATE INDEX "search_event_names" ON "event" USING GIST(to_tsvector('english', "name"));
