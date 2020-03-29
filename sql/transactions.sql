a43f425fce652fa9b23eac18a993d9cf09fd9a94
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-03-29 13:48:21 +0100

    Nao pintou

--- sql/transactions.sql
+++ sql/transactions.sql
@@ -1,16 +1,15 @@
-BEGIN TRANSACTION;
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ 
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ 
  
 -- Insert user
-INSERT INTO "user" ("name", "email", "password") 
-    VALUES ($name, $email, $password; 
+ INSERT INTO "user" ("name", "email", "password") 
+  VALUES ($name, $email, $password; 
  
 -- Insert regular_user 
-INSERT INTO "regular_user" ("user_id", "personal_info") 
-    VALUES (currval('user_id_seq'), $info); 
+ INSERT INTO "regular_user" ("user_id", "personal_info") 
+  VALUES (currval('user_id_seq'), $info); 
 
 --Insert student
-INSERT INTO "student" ("regular_user_id")
-    VALUES (currval('regular_user_id_seq'));
+ INSERT INTO "student" ("regular_user_id")
+  VALUES (currval('regular_user_id_seq'));
 
 COMMIT;
