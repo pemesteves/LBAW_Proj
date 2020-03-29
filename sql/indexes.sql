2178b5eef871993add64cfc481860ba238de0ec0
Pedro Esteves <up201705160@fe.up.pt>
2020-03-29 15:04:08 +0100

    Added more indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -3,6 +3,9 @@ DROP INDEX IF EXISTS "user_email";
 DROP INDEX IF EXISTS "student_regular_id";
 DROP INDEX IF EXISTS "teacher_regular_id";
 DROP INDEX IF EXISTS "organization_regular_id";
+DROP INDEX IF EXISTS "post_author_id";
+DROP INDEX IF EXISTS "post_event_id";
+DROP INDEX IF EXISTS "post_group_id";
 DROP INDEX IF EXISTS "search_post_titles";
 DROP INDEX IF EXISTS "search_user_names";
 DROP INDEX IF EXISTS "search_group_names";
@@ -17,6 +20,10 @@ CREATE INDEX "student_regular_id" ON "student" USING hash("regular_user_id");
 CREATE INDEX "teacher_regular_id" ON "teacher" USING hash("regular_user_id");
 CREATE INDEX "organization_regular_id" ON "organization" USING hash("regular_user_id");
 CREATE INDEX "post_author_id" ON "post" USING hash("author_id");
+
+-- POST FOREIGN KEYS INDEXES
+CREATE INDEX "post_event_id" ON "post" USING hash("event_id") WHERE "event_id" IS NOT NULL;
+CREATE INDEX "post_group_id" ON "post" USING hash("group_id") WHERE "group_id" IS NOT NULL; 
 
 -- GIST SEARCH INDEXES 
 CREATE INDEX "search_post_titles" ON "post" USING GIST(to_tsvector('english', "title"));
