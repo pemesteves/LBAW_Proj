7005a3a090a58f5b6ad5f3de30f4e52fcb72e38c
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2020-04-09 19:24:26 +0100

    DB done, transactions corrected

--- sql/db.sql
+++ sql/db.sql
@@ -489,3 +489,53 @@ LANGUAGE plpgsql;
 CREATE TRIGGER unique_org
     BEFORE UPDATE ON public."organization"
     EXECUTE PROCEDURE unique_org();
+
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
+ 
+-- Insert user
+ INSERT INTO "user" ("name", "email", "password") 
+  VALUES ($name, $email, $password); 
+ 
+-- Insert regular_user 
+ INSERT INTO "regular_user" ("user_id", "personal_info") 
+  VALUES (currval('user_user_id_seq'), $info); 
+
+--Insert student
+ INSERT INTO "student" ("regular_user_id")
+  VALUES (currval('regular_user_regular_user_id_seq'));
+
+COMMIT;
+
+
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
+ 
+-- Insert user
+ INSERT INTO "user" ("name", "email", "password") 
+  VALUES ($name, $email, $password); 
+ 
+-- Insert regular_user 
+ INSERT INTO "regular_user" ("user_id", "personal_info") 
+  VALUES (currval('user_user_id_seq'), $info); 
+
+--Insert teacher
+ INSERT INTO "teacher" ("regular_user_id")
+  VALUES (currval('regular_user_regular_user_id_seq'));
+
+COMMIT;
+
+
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
+ 
+-- Insert user
+ INSERT INTO "user" ("name", "email", "password") 
+  VALUES ($name, $email, $password); 
+ 
+-- Insert regular_user 
+ INSERT INTO "regular_user" ("user_id", "personal_info") 
+  VALUES (currval('user_user_id_seq'), $info); 
+
+--Insert organization
+ INSERT INTO "organization" ("regular_user_id")
+  VALUES (currval('regular_user_regular_user_id_seq'));
+
+COMMIT;
--- sql/transactions.sql
+++ sql/transactions.sql
@@ -1,49 +1,50 @@
-BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ 
+ROLLBACK;
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
  
 -- Insert user
  INSERT INTO "user" ("name", "email", "password") 
-  VALUES ($name, $email, $password; 
+  VALUES ($name, $email, $password); 
  
 -- Insert regular_user 
  INSERT INTO "regular_user" ("user_id", "personal_info") 
-  VALUES (currval('user_id_seq'), $info); 
+  VALUES (currval('user_user_id_seq'), $info); 
 
 --Insert student
  INSERT INTO "student" ("regular_user_id")
-  VALUES (currval('regular_user_id_seq'));
+  VALUES (currval('regular_user_regular_user_id_seq'));
 
 COMMIT;
 
 
-BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ 
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
  
 -- Insert user
  INSERT INTO "user" ("name", "email", "password") 
-  VALUES ($name, $email, $password; 
+  VALUES ($name, $email, $password); 
  
 -- Insert regular_user 
  INSERT INTO "regular_user" ("user_id", "personal_info") 
-  VALUES (currval('user_id_seq'), $info); 
+  VALUES (currval('user_user_id_seq'), $info); 
 
 --Insert teacher
  INSERT INTO "teacher" ("regular_user_id")
-  VALUES (currval('regular_user_id_seq'));
+  VALUES (currval('regular_user_regular_user_id_seq'));
 
 COMMIT;
 
 
-BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ 
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ; 
  
 -- Insert user
  INSERT INTO "user" ("name", "email", "password") 
-  VALUES ($name, $email, $password; 
+  VALUES ($name, $email, $password); 
  
 -- Insert regular_user 
  INSERT INTO "regular_user" ("user_id", "personal_info") 
-  VALUES (currval('user_id_seq'), $info); 
+  VALUES (currval('user_user_id_seq'), $info); 
 
 --Insert organization
  INSERT INTO "organization" ("regular_user_id")
-  VALUES (currval('regular_user_id_seq'));
+  VALUES (currval('regular_user_regular_user_id_seq'));
 
 COMMIT;
