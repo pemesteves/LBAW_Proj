1c231e06e852c03a6b715d8ac96417d8e488626d
Gustavo Torres <torresgustam@gmail.com>
2020-04-19 15:34:18 +0100

    posts improved, now with comments

--- app/Post.php
+++ app/Post.php
@@ -42,16 +42,17 @@ class Post extends Model
       
   ];
 
+  protected $with = ['user'];
 
   /**
    * The user this card belongs to
    */
   public function user() {
-    return $this->belongsTo('App\User');
+    return $this->belongsTo('App\User' , 'author_id');
   }
 
   public function comments(){
-    return $this->hasMany('App\Comment');
+    return $this->hasMany('App\Comment' , 'comment_id');
   }
 
 
--- resources/views/partials/comment.blade.php
+++ resources/views/partials/comment.blade.php
@@ -1,14 +1,16 @@
+
+
 <div class="row comment_container <?= $comment['padding_left'] != 0 ? 'comment_padding' : 'comment_no_padding' ?>">
     <div class="col-2 comment_user_info" >
         <div class="row">   
             <img src="https://www.pluspixel.com.br/wp-content/uploads/avatar-7.png" class="mx-auto d-block" alt="..." style="border-radius:50%; max-width:2rem; "  onclick="window.location.href='./profile.php'">
         </div>
         <div class="row">
-            <h4 style="font-size: 1em; margin: 0 auto;">{{ $comment['author'] }}</h4>
+            <h4 style="font-size: 1em; margin: 0 auto;">{{ $comment->user->name }}</h4>
         </div>
     </div>
     <div class="col-9 comment_text">
-        <p>{{ $comment['comment'] }}</p>
+        <p>{{ $comment->body }}</p>
     </div>
     <div class="col-1 comment_opt">
         <button><span class="fa fa-ellipsis-v"></span></button>
