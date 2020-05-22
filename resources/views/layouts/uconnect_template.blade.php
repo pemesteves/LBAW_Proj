61513587f24d4d5023ee040d5ebdec1aedd3d492
BerserkingIdiot <skullventuzelos@hotmail.com>
2020-05-22 10:49:52 +0100

    fixed broken feedback on post requests

--- resources/views/layouts/uconnect_template.blade.php
+++ resources/views/layouts/uconnect_template.blade.php
@@ -72,7 +72,7 @@
                 
             @if(Session::has("success_message"))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
-                <?php 
+                <?=
                 Session::get("success_message") 
                 ?>
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
